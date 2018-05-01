74c40bd8bbdaa6f165ce8a00d8a73829511ea796
CSMaris <catalinstefan.maris@studenti.polito.com>
2018-05-01 21:22:58 +0200

    Creazione cartella progetto

---
+++ LineaProduzione/src/model/Main.java
@@ -0,0 +1,35 @@
+package model;
+
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("InterfacciaGrafica.fxml"));
+			BorderPane root = (BorderPane) loader.load();
+
+			
+
+			Scene scene = new Scene(root);
+			
+			
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
